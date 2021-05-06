ab80e6d6dac158cff9b8db959f990fea0976c883
SrimayeeP <srimayee.ponuganti@kinandcarta.com>
2021-05-06 10:17:27 -0500

    added url

--- src/main/java/com/kinandcarta/testdd/controller/PersonController.java
+++ src/main/java/com/kinandcarta/testdd/controller/PersonController.java
@@ -1,11 +1,13 @@
 package com.kinandcarta.testdd.controller;
 
 import com.kinandcarta.testdd.model.Person;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
 
+    @GetMapping("/person")
     public Person getPerson(){
         Person person = new Person();
         person.setName("test");
